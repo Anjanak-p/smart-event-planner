0642e4db853658f1aa86a9cbd49a189918221ead
Anjanak-p <anjanaramachandran04@gmail.com>
2025-11-21 21:27:22 +0530

    changes

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -49,21 +49,6 @@ api.interceptors.response.use(
 );
 
 
-
-// Response interceptor to handle errors
-/*api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      // Token expired or invalid
-      localStorage.removeItem('token');
-      localStorage.removeItem('user');
-      window.location.href = '/login';
-    }
-    return Promise.reject(error);
-  }
-); */
-
 // API methods
 export const authAPI = {
   login: (credentials) => api.post('/users/login', credentials),
